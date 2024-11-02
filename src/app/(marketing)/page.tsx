bb54178a3e2645738dd88e8b2e8dcf2b45d824ff
fazlay <rfazlay21@gmail.com>
2024-11-02 23:52:55 +0600

    update

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,41 @@
-export default function Page() {
+// pages/index.js
+"use client";
+import { useEffect, useState } from "react";
+
+import socket from "@/lib/socket";
+
+const Home = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Listen for messages from the server
+    socket.on("message", (data) => {
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+
+    // Clean up the socket connection on component unmount
+    return () => {
+      socket.off("message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    const message = { text: "Hello from Next.js!" };
+    socket.emit("message", message);
+  };
+
   return (
     <div>
-      <h1 className='text-4xl font-bold'>Anonymous Voice ✨</h1>
+      <h1>Socket.IO with Next.js</h1>
+      <button onClick={sendMessage}>Send Message</button>
+      <h2>Messages:</h2>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg.text}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
