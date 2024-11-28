50d8dca526465ac65975538aae41896aab155fad
solaimanb <solaimanb.dev@gmail.com>
2024-11-28 17:30:45 +0600

    feat: integrate Lenis smooth scroll

---
+++ src/lib/lenis.ts
@@ -0,0 +1,20 @@
+import Lenis from "@studio-freight/lenis";
+
+export const initLenis = () => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    orientation: "vertical",
+    smoothWheel: true,
+    touchMultiplier: 2,
+    wheelMultiplier: 1,
+  });
+
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+  return lenis;
+};
